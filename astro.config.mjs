e1e8ed6c979baeed8285359fe4390336a89a3301
Anuj Paudel <anujpaudel98@yahoo.com>
2025-02-21 15:49:40 +0545

    feat: switch deployment adapter from Vercel to Netlify and update .gitignore

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify/functions";
 
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: netlify(),
   integrations: [
     tailwind()
   ],
