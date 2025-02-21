6fc7148816334597dc096211366e2eba520d2cc9
Anuj Paudel <anujpaudel98@yahoo.com>
2025-02-21 18:20:48 +0545

    feat: enable edge middleware in Netlify adapter and update build configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
 
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: netlify(),
+  adapter: netlify({
+    edgeMiddleware: true // Enable edge middleware
+  }),
   integrations: [
     tailwind()
   ]
